a8fbd98ee67224b76f5ac9ef9bec101ce20bdb3e
alok-kumar21 <alokprajapati021@gmail.com>
2025-09-10 20:13:47 +0530

    dfd

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -199,7 +199,7 @@ const Dashboard = () => {
                               key={task._id}
                               className="list-group-item d-flex justify-content-between align-items-center"
                             >
-                              {task?.tags === "To Do" ? task.name : "df"}
+                              {task?.name}
                             </li>
                           ))}
                         </ul>
